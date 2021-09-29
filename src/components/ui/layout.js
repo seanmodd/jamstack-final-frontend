327a987d618d5ee2c9c0a0a9ba843ad98a9a003f
Sean <>
2021-09-29 16:09:26 -0700

    updated gatsby from 2.24.61 to 3.14.0

---
+++ src/components/ui/layout.js
@@ -0,0 +1,61 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+
+import Header from "./header"
+import Footer from "./footer"
+
+const useStyles = makeStyles(theme => ({
+  spacer: {
+    marginBottom: "5rem",
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "2rem",
+    },
+  },
+}))
+
+const Layout = ({ children }) => {
+  const classes = useStyles()
+
+  // const data = useStaticQuery(graphql`
+  //   query GetCategories {
+  //     allStrapiCategory {
+  //       edges {
+  //         node {
+  //           name
+  //           strapiId
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+const data = useStaticQuery(graphql`
+query GetCategories {
+  categories {
+    id
+    name
+  }
+}
+`)
+
+
+
+  return (
+    <>
+      <Header categories={data.categories.edges} />
+      <div className={classes.spacer} />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
