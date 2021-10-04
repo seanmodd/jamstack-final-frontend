6fa1fc6a21825874d6dd666edac85878a981a53f
Sean <>
2021-10-04 11:47:08 -0700

    added a comment to clarify that the custom hook useIsClient.js is created specifically to fix all Rehydration errors

--- src/hooks/useIsClient.js
+++ src/hooks/useIsClient.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react"
+//! This is specifically to solve Rehydration errors!
+import { useState, useEffect } from 'react'
 
 export const useIsClient = () => {
   const [isClient, setIsClient] = useState(false)
-  const key = isClient ? "client" : "server"
+  const key = isClient ? 'client' : 'server'
 
   useEffect(() => {
     setIsClient(true)
